2a679e81eeac21defb01198e48e5f9c58c13a107
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-09-04 02:08:11 +0200

    Implement the party form

--- src/app/archinputform/archinputform.ts
+++ src/app/archinputform/archinputform.ts
@@ -1,6 +1,15 @@
 import { Component } from '@angular/core';
 import { FillingStrategy } from 'parliamentarch/geometry';
 
+interface Party {
+    readonly nId: number;
+    name: string;
+    nSeats: number;
+    fillColor: string;
+    borderWidth: number;
+    borderColor: string;
+}
+
 @Component({
   selector: 'app-archinputform',
   imports: [],
@@ -10,11 +19,54 @@ import { FillingStrategy } from 'parliamentarch/geometry';
 export class Archinputform {
     // imports for the template
     FillingStrategy = FillingStrategy;
+    isInt = Number.isInteger;
 
     fillingStrategy = FillingStrategy.DEFAULT;
     isAdvancedHidden = true;
+    readonly partylist: Party[] = [];
 
     toggleAdvanced() {
         this.isAdvancedHidden = !this.isAdvancedHidden;
+    }
+
+    addPartyManual() {
+        this.addParty();
+    }
+
+    deleteParty(nId: number) {
+        const index = this.partylist.findIndex(party => party.nId === nId);
+        this.partylist.splice(index, 1);
+    }
+
+    makeDiagram() {
+        this.callDiagramScript();
+    }
+
+    private addParty({
+        newName = "",
+        newColor = "#c60", // TODO default to randomcolor
+        newNSeats = 0,
+    } = {}) {
+        let nId = 1;
+        for (const party of this.partylist) {
+            if (party.nId >= nId) {
+                nId = party.nId + 1;
+            }
+        }
+
+        newName ||= `Party ${nId}`;
+
+        this.partylist.push({
+            nId,
+            name: newName,
+            nSeats: newNSeats,
+            fillColor: newColor,
+            borderWidth: 0,
+            borderColor: "#000000",
+        });
+    }
+
+    private callDiagramScript() {
+        // TODO
     }
 }
