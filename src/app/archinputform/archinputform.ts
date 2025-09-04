c687697eac1c8b9e938a6267abdfcdf759a002e0
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-09-04 05:06:35 +0200

    Make archinputform functional
    with syntax warnings

--- src/app/archinputform/archinputform.ts
+++ src/app/archinputform/archinputform.ts
@@ -1,4 +1,4 @@
-import { Component, computed, signal, WritableSignal } from '@angular/core';
+import { Component, computed, ElementRef, inject, Renderer2, signal, WritableSignal } from '@angular/core';
 import { getSVGFromAttribution } from 'parliamentarch';
 import { FillingStrategy } from 'parliamentarch/geometry';
 
@@ -24,9 +24,13 @@ export class Archinputform {
 
     fillingStrategy = FillingStrategy.DEFAULT;
     isAdvancedHidden = true;
-    readonly partylist: Party[] = [];
+    readonly partylist = signal<readonly Party[]>([]);
     readonly totalSeats = computed(() =>
-        this.partylist.reduce((sum, party) => sum + party.nSeats(), 0));
+        this.partylist().reduce((sum, party) => sum + party.nSeats(), 0));
+    readonly svgs: SVGSVGElement[] = [];
+
+    readonly renderer = inject(Renderer2);
+    readonly elementRef = inject<ElementRef<Element>>(ElementRef);
 
     toggleAdvanced() {
         this.isAdvancedHidden = !this.isAdvancedHidden;
@@ -37,12 +41,16 @@ export class Archinputform {
     }
 
     deleteParty(nId: number) {
-        const index = this.partylist.findIndex(party => party.nId === nId);
-        this.partylist.splice(index, 1);
+        const newPartylist = this.partylist().filter(party => party.nId !== nId);
+        this.partylist.set(newPartylist);
     }
 
     makeDiagram() {
         this.callDiagramScript();
+    }
+
+    downloadSVG(svg: SVGSVGElement) {
+        // TODO
     }
 
     private addParty({
@@ -50,8 +58,9 @@ export class Archinputform {
         newColor = "#c60", // TODO default to randomcolor
         newNSeats = 0,
     } = {}) {
+        const partylist = this.partylist();
         let nId = 1;
-        for (const party of this.partylist) {
+        for (const party of partylist) {
             if (party.nId >= nId) {
                 nId = party.nId + 1;
             }
@@ -59,17 +68,36 @@ export class Archinputform {
 
         newName ||= `Party ${nId}`;
 
-        this.partylist.push({
+        const newParty = {
             nId,
             name: signal(newName),
             nSeats: signal(newNSeats),
             fillColor: signal(newColor),
             borderWidth: signal(0),
             borderColor: signal("#000000"),
-        });
+        };
+        this.partylist.set(partylist.concat([newParty]));
     }
 
     private callDiagramScript() {
-        // TODO
+        // TODO create mediawiki markup legend
+        const attrib = new Map(this.partylist().map(party => [{
+            id: party.nId.toFixed(),
+            data: party.name(),
+            color: party.fillColor(),
+            borderSize: party.borderWidth(),
+            borderColor: party.borderColor(),
+        }, party.nSeats()]));
+
+        // TODO add the other options here to the advanced options form
+        const arch = getSVGFromAttribution(attrib, undefined, { fillingStrategy: this.fillingStrategy }, {});
+        this.svgs.unshift(arch);
+    }
+
+    setPFirstChild(p: HTMLParagraphElement, svg: SVGSVGElement) {
+        const firstChild = p.firstChild;
+        if (firstChild !== svg) {
+            p.insertBefore(svg, firstChild);
+        }
     }
 }
