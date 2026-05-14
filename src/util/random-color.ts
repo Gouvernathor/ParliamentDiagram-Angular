53041f856931a73ceed1fd06c57e01733f0778d0
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2026-05-14 15:18:09 +0200

    clean house

--- src/util/random-color.ts
+++
@@ -1,8 +0,0 @@
-const letters = '0123456789ABCDEF';
-export function randomColor(): string {
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
