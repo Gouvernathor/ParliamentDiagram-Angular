e18d51c286f4572945ff1ec2d67e0a475a84411c
Gouvernathor <Gouvernathor@noreply.codeberg.org>
2026-05-14 19:19:44 +0200

    create new parties

--- src/app/+arch-input-form/arch-input-form.ts
+++ src/app/+arch-input-form/arch-input-form.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
-import { StandardPage } from '../shared/standard-page/standard-page';
+import { Component, inject, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { StandardPage } from '../shared/standard-page/standard-page';
+import { ColorService } from '../shared/color-service';
+
+interface Party {
+    name: string;
+    nSeats: number;
+    color: string;
+    borderWidth: number;
+    borderColor: string;
+}
 
 @Component({
-  imports: [StandardPage, MatButtonModule],
-  templateUrl: './arch-input-form.html',
-  styleUrl: './arch-input-form.scss',
+    imports: [StandardPage, MatButtonModule],
+    templateUrl: './arch-input-form.html',
+    styleUrl: './arch-input-form.scss',
 })
-export class ArchInputFormPage {}
+export class ArchInputFormPage {
+    private readonly colorService = inject(ColorService);
+    protected readonly parties = signal<readonly Readonly<Party>[]>([]);
+
+    protected addParty() {
+        const party = this.newParty();
+
+        this.parties.set(this.parties().concat([party]));
+    }
+
+    private newParty(): Party {
+        return {
+            name: "",
+            nSeats: 0,
+            color: this.colorService.randomColor(),
+            borderWidth: 0,
+            borderColor: "black",
+        };
+    }
+}
---
+++ src/app/shared/color-service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from "@angular/core";
+
+@Injectable({
+    providedIn: "root",
+})
+export class ColorService {
+    private static readonly LETTERS: readonly string[] = "0123456789ABCDEF".split("");
+
+    /**
+     * Does not include the starting #
+     */
+    randomColor() {
+        return Array.from({ length: 6 }, () => ColorService.LETTERS[Math.floor(Math.random() *16)])
+            .join("");
+    }
+}
