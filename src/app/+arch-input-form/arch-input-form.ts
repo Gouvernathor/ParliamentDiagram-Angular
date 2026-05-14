c32d8f776055fd380e19e494f9d184b89cd9cb14
Gouvernathor <Gouvernathor@noreply.codeberg.org>
2026-05-14 18:49:07 +0200

    archinputform page skeleton

--- src/app/+arch-input-form/arch-input-form.ts
+++ src/app/+arch-input-form/arch-input-form.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { StandardPage } from '../shared/standard-page/standard-page';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
-  imports: [StandardPage],
+  imports: [StandardPage, MatButtonModule],
   templateUrl: './arch-input-form.html',
   styleUrl: './arch-input-form.scss',
 })
