b391a59cb29b1bc949654e9ac9a315beba35ec7d
Gouvernathor <Gouvernathor@noreply.codeberg.org>
2026-05-14 15:44:37 +0200

    Add main page

---
+++ src/app/+arch-input-form/arch-input-form.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  templateUrl: './arch-input-form.html',
+  styleUrl: './arch-input-form.scss',
+})
+export class ArchInputFormPage {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { ArchInputFormPage } from './+arch-input-form/arch-input-form';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        redirectTo: "/archinputform",
+        pathMatch: "full",
+    },
+    {
+        path: "archinputform",
+        component: ArchInputFormPage,
+        title: "Arch-style parliament diagram generator",
+    },
+];
