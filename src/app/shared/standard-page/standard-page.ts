3691e242bc9b60d2ae543419f871499ae36173f8
Gouvernathor <Gouvernathor@noreply.codeberg.org>
2026-05-14 16:32:08 +0200

    Add the header component

--- src/app/shared/standard-page/standard-page.ts
+++ src/app/shared/standard-page/standard-page.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Header } from '../header/header';
 
 @Component({
   selector: 'app-standard-page',
-  imports: [],
+  imports: [Header],
   templateUrl: './standard-page.html',
   styleUrl: './standard-page.scss',
 })
