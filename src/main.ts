7a1030d9d08d474fe54e459ff65b8b159535fed7
Gouvernathor <Gouvernathor@noreply.codeberg.org>
2026-05-15 22:58:58 +0200

    move the import

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,8 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
+// web-component
+import "hdr-color-input";
+
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
