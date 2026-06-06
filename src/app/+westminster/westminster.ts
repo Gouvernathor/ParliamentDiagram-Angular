e975503c30f3530a4cc825ca4df5e6546617143e
Gouvernathor <Gouvernathor@noreply.codeberg.org>
2026-06-06 16:41:36 +0200

    add westminster page

---
+++ src/app/+westminster/westminster.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+import { StandardPage } from "../shared/standard-page/standard-page";
+
+@Component({
+    imports: [StandardPage],
+    templateUrl: "./westminster.html",
+    styleUrl: "./westminster.scss",
+})
+export class WestminsterPage {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { ArchPage } from './+arch/arch';
+import { WestminsterPage } from './+westminster/westminster';
 
 export const routes: Routes = [
     {
@@ -7,6 +8,7 @@ export const routes: Routes = [
         redirectTo: "/arch",
         pathMatch: "full",
     },
+
     {
         path: "archinputform",
         redirectTo: "/arch",
@@ -15,5 +17,15 @@ export const routes: Routes = [
         path: "arch",
         component: ArchPage,
         title: "Arch-style parliament diagram generator",
+    },
+
+    {
+        path: "westminsterinputform",
+        redirectTo: "/westminster",
+    },
+    {
+        path: "westminster",
+        component: WestminsterPage,
+        title: "Westminster-style parliament diagram generator",
     },
 ];
