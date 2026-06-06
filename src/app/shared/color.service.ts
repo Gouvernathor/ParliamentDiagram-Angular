e93f5169cbe000af9399dfc8f643c7628f403df8
Gouvernathor <Gouvernathor@noreply.codeberg.org>
2026-06-06 17:04:15 +0200

    update

--- src/app/shared/color.service.ts
+++ src/app/shared/color.service.ts
@@ -6,9 +6,6 @@ import { Injectable } from "@angular/core";
 export class ColorService {
     private static readonly LETTERS: readonly string[] = "0123456789ABCDEF".split("");
 
-    /**
-     * Does not include the starting #
-     */
     randomColor() {
         return "#" + Array.from({ length: 6 }, () => ColorService.LETTERS[Math.floor(Math.random() *16)])
             .join("");
