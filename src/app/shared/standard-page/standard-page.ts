6af46e28855965c7cffe57006fd938c2f6197061
Gouvernathor <Gouvernathor@noreply.codeberg.org>
2026-06-20 12:16:45 +0200

    set up the theme-changing buttons

--- src/app/shared/standard-page/standard-page.ts
+++ src/app/shared/standard-page/standard-page.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { MatButtonToggleModule } from "@angular/material/button-toggle";
 import { MatCardModule } from '@angular/material/card';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { ThemeingService } from '../themeing';
 
 @Component({
-  selector: 'app-standard-page',
-  imports: [RouterLink, MatCardModule, MatToolbarModule],
-  templateUrl: './standard-page.html',
-  styleUrl: './standard-page.scss',
+    selector: 'app-standard-page',
+    imports: [RouterLink, MatCardModule, MatToolbarModule, MatButtonToggleModule],
+    templateUrl: './standard-page.html',
+    styleUrl: './standard-page.scss',
 })
-export class StandardPage {}
+export class StandardPage {
+    private readonly themeingService = inject(ThemeingService);
+}
