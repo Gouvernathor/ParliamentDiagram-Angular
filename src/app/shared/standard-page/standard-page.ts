5f7cc8559b2002385234fde6c218eb06aee5fa28
Gouvernathor <Gouvernathor@noreply.codeberg.org>
2026-06-20 10:34:42 +0200

    add nav

--- src/app/shared/standard-page/standard-page.ts
+++ src/app/shared/standard-page/standard-page.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-standard-page',
-  imports: [MatToolbarModule],
+  imports: [RouterLink, MatCardModule, MatToolbarModule],
   templateUrl: './standard-page.html',
   styleUrl: './standard-page.scss',
 })
