358e109831289e5396a679a4a53798d6e64dea75
Gouvernathor <Gouvernathor@noreply.codeberg.org>
2026-07-04 23:00:15 +0200

    simplify

--- Angular/src/app/shared/oauth/session.service.ts
+++ Angular/src/app/shared/oauth/session.service.ts
@@ -2,23 +2,18 @@ import { Injectable } from "@angular/core";
 import { OAuthCredentials, OAuthSession } from "@gouvernathor/m3api-oauth2";
 import { CompletedSession } from "./upload";
 
-interface Credentials {
-    readonly key: string;
-    readonly secret?: string;
-}
-
 @Injectable({
     providedIn: "root",
 })
 export class SessionService {
-    static readonly selfRestrictedCredentials: Credentials = {
-        key: "7fbac585c1bfcd02898ca7ce3e8041b0",
-        secret: "8850f9c90952fbfd854a3b442b9ceb78c15b82da",
-    };
-    static readonly localCredentials: Credentials = {
-        key: "3169b028c9dab61c11f925ab307a6aec",
-        secret: "5a7d8c3c505b31387eace86631937e42e225f975",
-    };
+    static readonly selfRestrictedCredentials = new OAuthCredentials(
+        "7fbac585c1bfcd02898ca7ce3e8041b0",
+        "8850f9c90952fbfd854a3b442b9ceb78c15b82da",
+    );
+    static readonly localCredentials = new OAuthCredentials(
+        "3169b028c9dab61c11f925ab307a6aec",
+        "5a7d8c3c505b31387eace86631937e42e225f975",
+    );
 
     private readonly apiUrl = "commons.wikimedia.org"; // not sure, maybe meta.wikimedia.org to auth first
     private readonly storageKey = "oauth-session";
@@ -26,17 +21,14 @@ export class SessionService {
     private credentials = SessionService.localCredentials;
     private session: InitedSession|CompletedSession|null = null;
 
-    private loadSession(credentials: Credentials) {
+    private loadSession(credentials: OAuthCredentials) {
         const serializationJson = globalThis.localStorage?.getItem(this.storageKey) ?? globalThis.sessionStorage?.getItem(this.storageKey);
         const serialization = serializationJson ? JSON.parse(serializationJson) : {};
 
         const session = new OAuthSession(this.apiUrl, {
             crossorigin: true,
         }, {
-            "m3api-oauth2/credentials": new OAuthCredentials(
-                credentials.key,
-                credentials.secret,
-            ),
+            "m3api-oauth2/credentials": credentials,
         }, serialization);
 
         return session;
@@ -63,7 +55,7 @@ export class SessionService {
         }
     }
 
-    private loadAndInit(credentials: Credentials) {
+    private loadAndInit(credentials: OAuthCredentials) {
         this.credentials = credentials;
         return this.init(this.loadSession(credentials));
     }
@@ -92,9 +84,9 @@ export class SessionService {
 
 /**
  * When you have this, the next step is to make the user click on the authorizationUrl.
- * The session is only useful to be passed to the complete method of the session service.
+ * It is also obtainable
  */
 export interface InitedSession {
     session: OAuthSession;
-    authorizationUrl: string;
+    authorizationUrl?: string;
 }
