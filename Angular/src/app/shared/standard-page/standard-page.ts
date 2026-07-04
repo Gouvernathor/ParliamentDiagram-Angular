ac52139d7cfa12015d612686f818676f64c72f7f
Gouvernathor <Gouvernathor@noreply.codeberg.org>
2026-07-05 01:33:31 +0200

    use the service in the standard page to show the link and status

--- Angular/src/app/shared/standard-page/standard-page.ts
+++ Angular/src/app/shared/standard-page/standard-page.ts
@@ -1,20 +1,25 @@
 import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from "@angular/material/button-toggle";
 import { MatCardModule } from '@angular/material/card';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { SessionService } from '../oauth/session.service';
 import { Theme, ThemeingService } from '../themeing';
 
 @Component({
     selector: 'app-standard-page',
-    imports: [RouterLink, MatCardModule, MatToolbarModule, MatButtonToggleModule],
+    imports: [RouterLink, MatCardModule, MatToolbarModule, MatButtonModule, MatButtonToggleModule],
     templateUrl: './standard-page.html',
     styleUrl: './standard-page.scss',
 })
 export class StandardPage {
     protected readonly themeingService = inject(ThemeingService);
+    protected readonly sessionService = inject(SessionService);
 
     protected chooseTheme(theme: Theme) {
         this.themeingService.setUserPersistedTheme(theme);
     }
+
+    protected readonly oauthEnabled = true; // TODO move to configuration
 }
