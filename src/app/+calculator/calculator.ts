025323f45a3ac83f3444a7df607da4bac849f953
Gouvernathor <Gouvernathor@noreply.codeberg.org>
2026-08-09 16:02:57 +0200

    create the calculator page

---
+++ src/app/+calculator/calculator.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+import { StandardPage } from "../shared/standard-page/standard-page";
+
+@Component({
+    imports: [StandardPage],
+    templateUrl: "./calculator.html",
+    styleUrl: "./calculator.scss",
+})
+export class CalculatorPage {}
