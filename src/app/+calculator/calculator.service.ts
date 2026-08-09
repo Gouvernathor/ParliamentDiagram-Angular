a838cf7e8bb37226116c64eb98f31fa729cfd600
Gouvernathor <Gouvernathor@noreply.codeberg.org>
2026-08-09 19:39:11 +0200

    compute the attrib (base case)

---
+++ src/app/+calculator/calculator.service.ts
@@ -0,0 +1,132 @@
+import { Service } from "@angular/core";
+import { getSVGFromAttribution, GetSVGFromAttributionOptions, SeatData } from "@parliamentarch/svg";
+import { FieldState } from "@angular/forms/signals";
+
+interface Party {
+    readonly name: string;
+    readonly nSeats: number;
+    readonly color: string;
+
+    readonly nYea: number;
+    readonly nNay: number;
+}
+
+export interface CalculatorData {
+    readonly parties: readonly Party[];
+
+    // calculation options
+    readonly majorityRatio: number | null;
+    readonly majorityThreshold: number | null;
+    readonly countAbstainAsAgainst: boolean;
+    readonly displayAbstainIfNotCountedAsAgainst: boolean;
+
+    readonly editPartyList: boolean; // TODO set to true by default, and to false when selecting a preset
+
+    // display options
+    readonly borderThickness: number; // maybe double this for Nay and Abstain separately
+}
+
+@Service()
+export class CalculatorService {
+    getDiagram(form: FieldState<CalculatorData>): Element|null {
+        if (!form.valid()) {
+            return null;
+        }
+
+        const value = form.value();
+        return getSVGFromAttribution(this.getAttrib(value), this.getOptions(value));
+    }
+
+    private getAttrib({
+        parties,
+        borderThickness,
+    }: Pick<CalculatorData, "parties"|"borderThickness">): ReadonlyMap<SeatData, number> {
+        const computedParties = parties.map(p => ({ nAbstain: p.nSeats-(p.nYea+p.nNay), ...p }));
+        const totalNAbstain = computedParties.reduce((a, p) => a + p.nAbstain, 0);
+        const nLeftAbstain = Math.trunc(totalNAbstain/2);
+
+        const yea = computedParties
+            .map<[SeatData, number]|null>(p => {
+                if (p.nYea <= 0) {
+                    return null;
+                }
+
+                return [this.yeaParty(p.name, p.color), p.nYea];
+            })
+            .filter(p => p != null);
+        const nay = computedParties
+            .map<[SeatData, number]|null>(p => {
+                if (p.nNay <= 0) {
+                    return null;
+                }
+
+                return [this.nayParty(p.name, p.color, borderThickness), p.nNay];
+            })
+            .filter(p => p != null);
+
+        const leftAbstain: [SeatData, number][] = [];
+        const rightAbstain: [SeatData, number][] = [];
+        let nPlacedLeftAbstain = 0;
+        const abstainPartiesToPop = computedParties.slice();
+        while (nPlacedLeftAbstain < nLeftAbstain) {
+            const party = abstainPartiesToPop.shift();
+            if (party == null) {
+                throw new Error("bad computation of the number of left-abstain parties");
+            }
+
+            if (party.nAbstain + nPlacedLeftAbstain > nLeftAbstain) {
+                const nPlacedLeft = nLeftAbstain - nPlacedLeftAbstain;
+                leftAbstain.push([this.abstainParty(party.name, party.color, borderThickness), nPlacedLeft]);
+                nPlacedLeftAbstain = nLeftAbstain;
+
+                // add the right part to rightAbstain
+                rightAbstain.push([this.abstainParty(party.name, party.color, borderThickness), party.nAbstain-nPlacedLeft]);
+            } else {
+                leftAbstain.push([this.abstainParty(party.name, party.color, borderThickness), party.nAbstain]);
+                nPlacedLeftAbstain += party.nAbstain;
+            }
+        }
+        // map the remaining abstainPartiesToPop and append them to rightAbstain
+        for (const party of abstainPartiesToPop) {
+            rightAbstain.push([this.abstainParty(party.name, party.color, borderThickness), party.nAbstain]);
+        }
+
+        return new Map([...leftAbstain, ...yea, ...nay, ...rightAbstain]);
+    }
+
+    private yeaParty(name: string, color: string): SeatData {
+        return {
+            data: name,
+            color,
+        };
+    }
+
+    private abstainParty(name: string, color: string, borderThickness: number): SeatData {
+        return {
+            data: name,
+            color: "transparent",
+            borderColor: color,
+
+            // TODO support dashed line in ParliamentArch,
+            // or set it using a class with ng-deep,
+            // and then remove this /2
+            borderSize: borderThickness/2,
+        };
+    }
+
+    private nayParty(name: string, color: string, borderThickness: number) {
+        return {
+            data: name,
+            color: "transparent",
+            borderColor: color,
+            borderSize: borderThickness,
+        };
+    }
+
+    private getOptions({
+    }: Pick<CalculatorData, never>): Partial<Readonly<GetSVGFromAttributionOptions>> {
+        // TODO
+        // majorityLineCheckpoints: nSeats, round, ratio
+        return {};
+    }
+}
--- src/app/+calculator/calculator.ts
+++ src/app/+calculator/calculator.ts
@@ -1,30 +1,9 @@
-import { Component, signal } from "@angular/core";
-import { form, max, min, minLength, validate } from "@angular/forms/signals";
+import { Component, computed, inject, signal } from "@angular/core";
+import { applyEach, form, max, min, minLength, validate } from "@angular/forms/signals";
 import { MatExpansionModule } from "@angular/material/expansion";
 import { StandardPage } from "../shared/standard-page/standard-page";
 import { Contents } from "../shared/contents.directive";
-
-interface Party {
-    readonly name: string;
-    readonly nSeats: number;
-    readonly color: string;
-}
-
-interface CalculatorData {
-    readonly parties: readonly Party[];
-
-    // calculation options
-    readonly majorityRatio: number|null;
-    readonly majorityThreshold: number|null;
-    readonly countAbstainAsAgainst: boolean;
-    readonly displayAbstainIfNotCountedAsAgainst: boolean;
-
-    readonly editPartyList: boolean; // TODO set to true by default, and to false when selecting a preset
-
-    // display options
-    readonly borderThickness: number; // maybe double this for Nay and Abstain separately
-    // …other global arch settings
-}
+import { CalculatorData, CalculatorService } from "./calculator.service";
 
 @Component({
     imports: [StandardPage, MatExpansionModule, Contents],
@@ -32,6 +11,8 @@ interface CalculatorData {
     styleUrl: "./calculator.scss",
 })
 export class CalculatorPage {
+    private readonly service = inject(CalculatorService);
+
     protected readonly form = form(signal<CalculatorData>({
         parties: [],
         majorityRatio: null,
@@ -42,6 +23,18 @@ export class CalculatorPage {
         borderThickness: .1, // TODO tweak
     }), schemaPath => {
         minLength(schemaPath.parties, 1);
+        applyEach(schemaPath.parties, party => {
+            validate(party, ({ value }) => {
+                const { name, nSeats, nYea, nNay } = value();
+                if (nSeats < nYea+nNay) {
+                    return {
+                        kind: "number of seats",
+                        message: `The number of expressed votes is higher than the number of seats for party ${name}`,
+                    };
+                }
+                return;
+            });
+        });
         validate(schemaPath.parties, ({ value }) => {
             const totalNSeats = value().reduce((a, p) => a + p.nSeats, 0);
             if (totalNSeats <= 0) {
@@ -57,5 +50,5 @@ export class CalculatorPage {
         max(schemaPath.borderThickness, 1);
     });
 
-    protected readonly diagram = signal<Element|null>(null); // TODO turn into a computed signal
+    protected readonly diagram = computed<Element|null>(() => this.service.getDiagram(this.form()));
 }
