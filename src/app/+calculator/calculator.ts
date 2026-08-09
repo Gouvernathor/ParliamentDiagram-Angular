d032bbccbe0c3b514f4bd63b941c2c9e75edeec9
Gouvernathor <Gouvernathor@noreply.codeberg.org>
2026-08-09 18:11:39 +0200

    prepare displaying the results in an arch

--- src/app/+calculator/calculator.ts
+++ src/app/+calculator/calculator.ts
@@ -1,10 +1,13 @@
-import { Component } from "@angular/core";
-import { StandardPage } from "../shared/standard-page/standard-page";
+import { Component, signal } from "@angular/core";
 import { MatExpansionModule } from "@angular/material/expansion";
+import { StandardPage } from "../shared/standard-page/standard-page";
+import { Contents } from "../shared/contents.directive";
 
 @Component({
-    imports: [StandardPage, MatExpansionModule],
+    imports: [StandardPage, MatExpansionModule, Contents],
     templateUrl: "./calculator.html",
     styleUrl: "./calculator.scss",
 })
-export class CalculatorPage {}
+export class CalculatorPage {
+    protected readonly diagram = signal<Element|null>(null); // TODO turn into a computed signal
+}
